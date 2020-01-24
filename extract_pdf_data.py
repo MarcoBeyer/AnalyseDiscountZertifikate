6cf92207ef55cd369a94992a6f9ffe014b45127e
marcobeyer <mbeyer2@gmail.com>
2020-01-24 16:17:16 +0100

    removed unneeded  lines of code

--- extract_pdf_data.py
+++ extract_pdf_data.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  2 09:32:08 2019
-
-@author: marco.beyer
-"""
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
@@ -24,7 +19,9 @@ def pdf_to_text(path):
         # PDFMiner boilerplate
         rsrcmgr = PDFResourceManager()
         sio = StringIO()
-        device = TextConverter(rsrcmgr, sio, codec='utf-8', laparams=LAParams(line_margin=100, word_margin=0.4))
+        device = TextConverter(rsrcmgr, sio, codec='utf-8', 
+                               laparams=LAParams(line_margin=100, 
+                                                 word_margin=0.4))
         interpreter = PDFPageInterpreter(rsrcmgr, device)
         # get text from file
         for page in PDFPage.get_pages(file):
@@ -40,50 +37,84 @@ def pdf_to_text(path):
 def extract_bib_informations(text):
     values = {}
     # Risikoklasse
-    risikoklasse = re.search(r"in[^d]*die[^R]*Risikoklasse[^0-7]*([0-7])", text, re.MULTILINE).group(1)
+    risikoklasse = re.search(r"in[^d]*die[^R]*Risikoklasse[^0-7]*([0-7])", 
+                             text).group(1)
     values['risikoklasse'] = risikoklasse
     # Einstiegskosten
     einstiegskosten = re.search(
-        r"(Einmalige Kosten|Einstiegskosten|inbegriffenen Kosten.|Auswirkung der im Preis bereits inbegriffenen \n)["
-        r"^0-9-]*([0-9,-]+)",
-        text, re.MULTILINE).group(2)
+        r"(Einmalige Kosten|Einstiegskosten|inbegriffenen Kosten.|"
+        r"Auswirkung der im Preis bereits inbegriffenen \n)[^0-9-]*([0-9,-]+)",
+        text).group(2)
     values['einstiegskosten'] = einstiegskosten
+    
     # Ausstiegskosten
     ausstiegskosten = re.search(
         r"(?<!Einstiegskosten\n)(?<!im Preis bereits )(?<!und )(inbegriffenen Kosten|Ausstiegskosten|Einmalige "
         r"Kosten\nweniger.)(?!.*\n*Einstiegskosten)[^,0-9-]*[^0-9-]*([0-9,-]+)",
-        text, re.MULTILINE).group(2)
+        text).group(2)
     values['ausstiegskosten'] = ausstiegskosten
+    
     # Erstelldatum
     erstelldatum = re.search(
         r"(Letzte Aktualisierung|datum|Erstellungszeit|Stand:|Überarbeitung des)[^0-9]*([0-9]*[0-9][.]*[^0-9]*["
         r"0-9.]+)(?!.*Letzte Aktualisierung)",
-        text, re.IGNORECASE | re.MULTILINE).group(2)
+        text, re.IGNORECASE).group(2)
     values['erstelldatum'] = erstelldatum
+    
     # Bezugsverhältnis
     bezugsverhaeltnis = re.search(
         r"((?<!dem |das )Bezugsverhältnis|Das Ergebnis wird mit)[^0-9.]*([0-9.,]* (Indexpunkte|EUR)\n|[0-9.]*\n)*(["
         r"0-9,]+)",
         text, re.MULTILINE).group(4)
     values['bezugsverhaeltnis'] = bezugsverhaeltnis
+    
+    # Fälligkeitsdatum
+    faelligkeitsdatum = re.sub(r'-','', text)
+    faelligkeitsdatum = re.sub(r' \(Fälligkeit\)', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r' /', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r':', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r'0,01\n', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r'Euro \(EUR\)\n', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r'Bewertungstag\n[^\n]*\n', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r'Ausgabe/Zahltag\nFälligkeitstag\n[^\n]*\n', 'Fälligkeitstag\n', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r' \(Fäl', '', faelligkeitsdatum)
+    faelligkeitsdatum = re.sub(r'Bezugsverhältnis\n', '', faelligkeitsdatum)
+    if("Der 5. Geschäftstag nach dem" in text):
+        faelligkeitsdatum = None
+    else:
+        faelligkeitsdatum = re.search(
+                r"(Rückzahlungstermin|voraussichtich spätestens der|Fälligkeitstag\n|Einlösungstermin\n|Am )[\n]*([0-9]*[0-9][.]*[^0-9]*[0-9.]+)",
+                faelligkeitsdatum).group(2)
+    values['faelligkeitsdatum'] = faelligkeitsdatum
+    
+    # Gesamtkosten
     gesamtkosten = re.search(
         r"(?<!EUR  )(?<!Haltedauer \(Fälligkeit\)\n)(?<!- )(?<!-)(?<!\)\)\n)(?<!0\n)(?<!EUR\n)(?<!EUR )"
         r"(Szenarien|einlösen|Gesamtkosten)\n*((EUR )*[0-9,-]+( EUR)*[^AG]*)", 
-        text, re.MULTILINE).group(2)
+        text).group(2)
     # remove new lines add the end
     gesamtkosten = gesamtkosten.replace(' ', '\n').replace('EUR', '').replace('\n\n', '\n').strip("\n\r").split('\n')
     for i in range(len(gesamtkosten)):
         values['gesamtkosten_' + str(i)] = gesamtkosten[len(gesamtkosten) - 1 - i]
+    
+    # Relative Gesamtkosten
+    # cleanup of special cases for easier regex
+    relative_gesamtkosten = re.sub(r'[0-9,.–-]+ EUR','\n', text)
+    relative_gesamtkosten = re.sub(r'Gesamtkosten','', relative_gesamtkosten)
+    relative_gesamtkosten = re.sub(r'\n[\n]+','\n', relative_gesamtkosten)
     relative_gesamtkosten = re.search(
         r"(?<!%\n)(Auswirkung auf die Rendite|Auswirkung auf die Rendite \(RIY\)|Auswirkung auf die Rendite \(RIY\) "
         r"pro|Auswirkung auf die Rendite \(RIY\) pro Jahr|Auswirkungen auf die Rendite \("
-        r"RIY\)|EUR\nGesamtkosten\nAuswirkung auf die Rendite \(RIY\) pro Jahr|EUR\nGesamtkosten|Auswirkung "
-        r"\n*auf|Auswirkungen auf die Rendite \(RIY\) pro Jahr) *\n*([0-9,%\n-]+[^A-Za-z\.(]*)",
-        text, re.MULTILINE).group(2)
+        r"RIY\)|einlösen\nSzenarien\n|Auswirkung \nauf|Auswirkungen auf die Rendite \(RIY\) "
+        r"pro Jahr) *\n*([0-9,%\n-]+[^A-Za-z\.(]*)",
+        relative_gesamtkosten).group(2)
     relative_gesamtkosten = relative_gesamtkosten.replace('%', '').replace(' ', '\n').replace('\n\n', '\n').strip(
         "\n\r").split('\n')
     for i in range(len(relative_gesamtkosten)):
         values['relative_gesamtkosten_' + str(i)] = relative_gesamtkosten[len(relative_gesamtkosten) - 1 - i]
+    print(relative_gesamtkosten)
+
+    # Mittleres Szenario
     #cleanup of special cases for easier regex
     mittleres_szenario = re.sub(r' EUR','\n', text)
     mittleres_szenario = re.sub(r'EUR ','', mittleres_szenario)
@@ -93,7 +124,7 @@ def extract_bib_informations(text):
     mittleres_szenario = re.sub(r'[0-9,-]*%\n', '', mittleres_szenario)
     mittleres_szenario = re.search(
         r"(Mittleres[ ]*(Szenario)?[\n]*([0-9.,]+[\n]*)+|([0-9.,]+[\n]*)+Mittleres[ ]*Szenario)",
-        mittleres_szenario, re.MULTILINE).group(1)
+        mittleres_szenario).group(1)
     mittleres_szenario = mittleres_szenario.replace(' ', '')\
                         .replace('Mittleres', '')\
                         .replace('Szenario', '')\
@@ -116,7 +147,7 @@ for file in files:
     print(str(i) + '/' + str(len(files)))
     print(file)
     try:
-        #file='DE000HX4D1G9.pdf'
+        #file='DE000HX65D37.pdf'
         text = pdf_to_text(path + "/" + file)
         # Text bereinigen
         text = text.replace('p. a.', '')
@@ -124,13 +155,14 @@ for file in files:
         text = re.sub(r'Seite [0-9]', '', text)
         text = re.sub(r'DocID:[^\n]*', '' , text)
         #remove empty lines
-        text = re.sub(r'^\n', '', text, flags = re.MULTILINE)
+        text = re.sub(r'^\n', '', text, flags=re.MULTILINE)
 
         informations = extract_bib_informations(text)
         informations['ISIN'] = file.strip('.pdf')
+        print(informations)
         data = data.append(informations, ignore_index=True)
     except:
         errors.append(file)
         print('Error in File ' + file)
-data.to_csv('BIB_Informationen_neu_2.csv', index=False)
+data.to_csv('BIB_Informationen_12_12.csv', index=False)
 print(errors)
